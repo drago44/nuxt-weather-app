c977aaafc1803c371e35150696b0850f88208f4b
Alex <oleksander.kucherenko.work@gmail.com>
2023-03-14 22:57:08 +0200

    change useFetch to useAsyncData add cookie, create env and runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      APP_SECRET: process.env.WEATHER_APP_SECRET,
+    },
+  },
   modules: ['@nuxtjs/tailwindcss'],
 });
